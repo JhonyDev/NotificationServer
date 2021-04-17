f7035316bec52c3f5de7701ce9c39661326766df
JhonyDev <star.jk901@gmail.com>
2021-04-18 02:33:34 +0500

    init

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Notification, NotificationPriority
+from .models import Notification, NotificationPriority, CronLogs
 
 admin.site.register(Notification)
 admin.site.register(NotificationPriority)
+admin.site.register(CronLogs)
