a728fd1b9fe4ad93574bf047c92a323543291bf8
JhonyDev <star.jk901@gmail.com>
2021-04-18 17:44:03 +0500

    init

--- api/CronJob.py
+++ api/CronJob.py
@@ -9,7 +9,7 @@ def push_notify(title):
         instance_id='1889a652-be8c-4e56-aed1-04bedd6eff47',
         secret_key='6274C8792B95D8C0A54DBE48ABFF7807DEEF94C6EFA83518E676280272254356',
     )
-    response = beams_client.publish(
+    response = beams_client.publish_to_interests(
         interests=['hello'],
         publish_body={
             'fcm': {
