c208edf63afe173ee717556665bc82f43305237b
JhonyDev <star.jk901@gmail.com>
2021-04-18 15:59:38 +0500

    init

--- api/CronJob.py
+++ api/CronJob.py
@@ -1,7 +1,33 @@
 from .models import CronLogs
 
 
-def my_cron_job():
+def push_notify():
+    from pusher_push_notifications import PushNotifications
+
+    beams_client = PushNotifications(
+        instance_id='1889a652-be8c-4e56-aed1-04bedd6eff47',
+        secret_key='6274C8792B95D8C0A54DBE48ABFF7807DEEF94C6EFA83518E676280272254356',
+    )
+    response = beams_client.publish_to_interests(
+        interests=['hello'],
+        publish_body={
+            'apns': {
+                'aps': {
+                    'alert': 'Hello!'
+                }
+            },
+            'fcm': {
+                'notification': {
+                    'title': 'Hello',
+                    'body': 'Hello, World!'
+                }
+            }
+        }
+    )
     cron = CronLogs()
-    cron.log_time = 'cron job inside'
+    cron.log_time = response['publishId']
     cron.save()
+
+
+def my_cron_job():
+    push_notify()
