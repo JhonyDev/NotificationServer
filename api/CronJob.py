6ae8f946aef39d36a047a5ce8fc89d0361fc894e
JhonyDev <star.jk901@gmail.com>
2021-04-22 02:01:24 +0500

    all done

--- api/CronJob.py
+++ api/CronJob.py
@@ -4,10 +4,11 @@ from .utils import check_for_updates
 
 def my_cron_job():
     user_list = list(Users.objects.all())
-
     for user in user_list:
         notification_priorities = list(NotificationPriority.objects.filter(user_id=user.get_user_token()))
         for notification_priority in notification_priorities:
+            print(user.get_user_token())
+            print(notification_priority.get_notification_id)
             check_for_updates(notification_priority.get_fixture_id(), user.get_user_token()
                               , notification_priority.get_notification_id())
 
