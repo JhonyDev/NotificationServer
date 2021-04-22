06f5e925061703ef8d9e685998c38419adeb0705
JhonyDev <star.jk901@gmail.com>
2021-04-22 17:20:18 +0500

    all done

--- api/utils.py
+++ api/utils.py
@@ -82,7 +82,7 @@ def red_card_notification(fixture_item, user_id):
             elapsed_time = str(event.get('elapsed'))
             break
         event_index += 1
-    title = 'Red Card ' + elapsed_time + ' min'
+    title = 'Red Card - ' + elapsed_time + ' min'
     subtitle2 = str(fixture_item.get('redCards'))
     push_notify(title, subtitle, subtitle2, info.RED_CARDS, user_id)
 
@@ -100,7 +100,7 @@ def yellow_card_notification(fixture_item, user_id):
             elapsed_time = str(event.get('elapsed'))
             break
         event_index += 1
-    title = 'Yellow Card ' + elapsed_time + ' min'
+    title = 'Yellow Card - ' + elapsed_time + ' min'
     subtitle = fixture_item.get('homeTeam').get('team_name') + ' v ' + fixture_item.get('awayTeam').get('team_name')
     subtitle2 = str(fixture_item.get('yellowCards'))
 
@@ -118,7 +118,7 @@ def goal_notification(fixture_item, user_id):
         if event.get('type') == info.GOAL:
             elapsed_time = str(event.get('elapsed'))
             break
-    title = 'Goal ' + elapsed_time + ' min'
+    title = 'Goal - ' + elapsed_time + ' min'
     subtitle = fixture_item.get('homeTeam').get('team_name') + ' v ' + fixture_item.get('awayTeam').get('team_name')
     push_notify(title, subtitle, '', info.GOALS, user_id)
 
