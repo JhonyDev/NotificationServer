39d1f5e91233ce929d7c8763bf08885d6b19a8c0
JhonyDev <star.jk901@gmail.com>
2021-04-23 15:36:34 +0500

    all done

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import SentNotification, NotificationPriority, CronLogs, Users
+from .models import SentNotification, NotificationPriority, CronLogs, Users, Fixtures
 
 admin.site.register(SentNotification)
 admin.site.register(NotificationPriority)
 admin.site.register(CronLogs)
 admin.site.register(Users)
+admin.site.register(Fixtures)
