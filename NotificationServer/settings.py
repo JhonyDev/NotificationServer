c83fce20caeab00c4dccfacbc269986dec1440da
JhonyDev <star.jk901@gmail.com>
2021-04-23 19:36:00 +0500

    all done

--- NotificationServer/settings.py
+++ NotificationServer/settings.py
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'NotificationServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'footballlive',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'Khhankhhiljy@12',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'footballlive',
-            'USER': 'myprojectuser',
-            'PASSWORD': 'Khhankhhiljy@12',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
