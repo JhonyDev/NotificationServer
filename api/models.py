db162f77cb86dc66eff89fc837540c8dbef0b08e
JhonyDev <star.jk901@gmail.com>
2021-05-01 00:52:21 +0500

    all done

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from api import info
 
 
 class NotificationPriority(models.Model):
+    id = models.AutoField(primary_key=True)
     first_notification = models.CharField(max_length=10, default=info.first)
     user_id = models.CharField(max_length=250, default='no_user')
     fixture_id = models.IntegerField()
