034f0f98193fe702e8a9db34b7c991f7d4b5024e
JhonyDev <star.jk901@gmail.com>
2021-05-12 03:52:59 +0500

    api changed

--- api/CronJob.py
+++ api/CronJob.py
@@ -4,6 +4,9 @@ from .models import Fixtures, CronLogs
 from .notification_utils import check_for_updates
 import time
 
+time_delay = 3
+loop_count = int(60 / time_delay)
+
 
 def my_cron_job():
     fixtures = list(Fixtures.objects.all())
@@ -12,24 +15,17 @@ def my_cron_job():
         check_for_updates(fixture.get_fixture_id())
 
 
-def run_cron():
+def run_cron_with(delay):
+    time.sleep(delay)
     my_cron_job()
+
+
+def run_cron():
     time_stamp = str(datetime.datetime.now())
     cron_log = CronLogs()
     cron_log.log_time = time_stamp
     cron_log.save()
 
-    time.sleep(10)
     my_cron_job()
-
-    time.sleep(10)
-    my_cron_job()
-
-    time.sleep(10)
-    my_cron_job()
-
-    time.sleep(10)
-    my_cron_job()
-
-    time.sleep(10)
-    my_cron_job()
+    for x in range(loop_count):
+        run_cron_with(time_delay)
