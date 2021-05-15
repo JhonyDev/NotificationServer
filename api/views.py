578f3121b8b1436a9cbc94c694fc8738d45405b5
JhonyDev <star.jk901@gmail.com>
2021-05-16 00:54:04 +0500

    api changed

--- api/views.py
+++ api/views.py
@@ -26,7 +26,7 @@ def current_milli_time():
 @api_view(['GET', ])
 def api_post_user_id(request2):
     user_id = str(current_milli_time())
-    user_id_in_query_param = request2.args.get('user_id')
+    user_id_in_query_param = request.args.get('user_id')
     print('###############' + user_id_in_query_param)
     if user_id != user_id_in_query_param:
         return 'Inconsistent request', 401
